9fde532a1a6cbc825b6ec43108b012a80edd4db0
Veranao <64379851+Veranao@users.noreply.github.com>
2025-06-21 00:47:40 +0000

    Added controller functionality for timeout

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -10,7 +10,17 @@ class Login extends Controller {
 			$username = $_REQUEST['username'];
 			$password = $_REQUEST['password'];
 
-		
+			if(isset($_SESSION['failedAuth']) && $_SESION['failedAuth'] >= 3) {
+				$elapsed_time = time() - $_SESSION['lastFailedTime'] ?? 0;
+				if ($elapsed_time < 120) {
+					$_SESSION['flash'] = "You have failed too many times. Please wait" + (120 - $elapsed_time) + "before you can try logging in again.";
+					header("Location: /login");
+					exit;
+				} else {
+					unset($_SESSION['failedAuth']);
+					unset($_SESSION['lastFailedTime']);
+				}
+			}
 			$user = $this->model('User');
 			$user->authenticate($username, $password); 
     }
